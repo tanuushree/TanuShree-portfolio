9562b127495c047754fa3973cefb56d0bdae9d6f
Tanu Shree <21cs240tanu@ug.cusat.ac.in>
2023-10-04 09:46:34 +0530

    updated stacks

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -19,6 +19,10 @@ function About(){
                     <div className="content" >
                         <p>I'm a pre-final year Computer Science student with a passion for technology. My journey has led me to explore web development, machine learning, and most recently the world of Web3. I've been a finalist in Smart India Hackathon Grand Finale 2022 and and have participated in EthIndia TPG Hackerhouse 2022. I've also authored two research papers, with hopes of seeing them published someday. Explore my work and join me in co-creating projects.
                         </p>
+                        <button class="button" type="button">
+                            <span class="button__text">Get    CV</span>
+                            <span class="button__icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 35 35" id="bdd05811-e15d-428c-bb53-8661459f9307" data-name="Layer 2" class="svg"><path d="M17.5,22.131a1.249,1.249,0,0,1-1.25-1.25V2.187a1.25,1.25,0,0,1,2.5,0V20.881A1.25,1.25,0,0,1,17.5,22.131Z"></path><path d="M17.5,22.693a3.189,3.189,0,0,1-2.262-.936L8.487,15.006a1.249,1.249,0,0,1,1.767-1.767l6.751,6.751a.7.7,0,0,0,.99,0l6.751-6.751a1.25,1.25,0,0,1,1.768,1.767l-6.752,6.751A3.191,3.191,0,0,1,17.5,22.693Z"></path><path d="M31.436,34.063H3.564A3.318,3.318,0,0,1,.25,30.749V22.011a1.25,1.25,0,0,1,2.5,0v8.738a.815.815,0,0,0,.814.814H31.436a.815.815,0,0,0,.814-.814V22.011a1.25,1.25,0,1,1,2.5,0v8.738A3.318,3.318,0,0,1,31.436,34.063Z"></path></svg></span>
+                        </button>
                     </div>
                 
                 
@@ -41,7 +45,9 @@ function About(){
                                             <div class="grid-item"><Icon className="icon" icon="simple-icons:solidity" color="#11232f" height="100" /><h5 style={{color:'#FF7545', fontFamily:'initial'}}>Solidity</h5></div>  
                                             <div class="grid-item"><Icon className="icon" icon="icomoon-free:html-five" color="#11232f" height="100" /><h5 style={{color:'#FF7545', fontFamily:'initial'}}>HTML</h5></div>
                                             <div class="grid-item"><Icon className="icon" icon="uiw:css3" color="#11232f" height="100" /><h5 style={{color:'#FF7545', fontFamily:'initial'}}>CSS</h5></div>
-                                            <div class="grid-item"><Icon className="icon" icon="teenyicons:react-outline" color="#11232f" height="100"/><h5 style={{color:'#FF7545', fontFamily:'initial'}}>React.js</h5></div>  
+                                            <div class="grid-item"><Icon className="icon" icon="teenyicons:react-outline" color="#11232f" height="100"/><h5 style={{color:'#FF7545', fontFamily:'initial'}}>React.js</h5></div> 
+                                            <div class="grid-item"><Icon icon="devicon-plain:mysql" color="#11232f" width="100" height="100" /><h5 style={{color:'#FF7545', fontFamily:'initial'}}>MySQL</h5></div>
+                                            <div class="grid-item"><Icon icon="simple-icons:firebase" color="#11232f" width="100" height="100" /><h5 style={{color:'#FF7545', fontFamily:'initial'}}>Firebase</h5></div> 
                                         </div>
                                         
                                     </div>
--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -7,12 +7,17 @@ function Home(){
     
   
     return(
-        <div>
-            <div className='first-page '>
+        <div className='work'>
+            < div className='first '>
                 
                 <div className='intro'>
+                    <h5 style={{fontFamily:'serif', fontWeight:'600'}}>My recent work</h5>
+                    <h6 style={{fontFamily:'serif', fontSize:'20px', color:'black'}}>These are some of the recent projects I have worked on. </h6>
                     
-                    
+                </div>
+                <div className="project">
+                        
+
                 </div>
             </div>
         </div>
